91467a2618882346228fee27f7f6b1dbfb042b2d
Diego <diegow88@gmail.com>
2016-11-18 00:13:38 -0500

    Sensor and location tracking works as well as pushing the content to AWS S3. In addition, I included Gradle files for the project

--- ProductStudio/app/src/main/java/com/bikenyc/productstudio/RecordActivity.java
+++ ProductStudio/app/src/main/java/com/bikenyc/productstudio/RecordActivity.java
@@ -1,20 +1,35 @@
 package com.bikenyc.productstudio;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 public class RecordActivity extends AppCompatActivity implements SensorEventListener {
     private boolean mRecording;
@@ -22,7 +37,9 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
     private SensorManager mSensorManager;
     private Sensor mSensorLinearAccelerometer;
     private Sensor mSensorGyroscope;
+
     private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
 
     private TrackingStorageManager mStorageManager;
 
@@ -37,6 +54,9 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
         // Get instance of the Sensor Manager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        // Get instance of the Location Manager
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         mStorageManager = new TrackingStorageManager();
 
         // Request access to the Linear Accelerometer
@@ -49,7 +69,6 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
         if (mSensorGyroscope == null)
             System.out.println("Gyroscope sensor not found");
 
-        // Request access to location manager
 
         this.mStartStopButton = (Button) findViewById(R.id.start_stop);
         this.mTrackButton = (Button) findViewById(R.id.track_double_parking);
@@ -84,14 +103,48 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
     }
 
     public void startRecording() {
-        this.mSensorManager.registerListener(this, mSensorGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-        //this.mSensorManager.registerListener(this, mSensorLinearAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        this.mSensorManager.registerListener(this, mSensorGyroscope, SensorManager.SENSOR_DELAY_GAME);
+        this.mSensorManager.registerListener(this, mSensorLinearAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+
+        mLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                mStorageManager.handleLocationEvent(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        if (
+                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("No permission to GPS");
+        }
+
+        // Register the listener with the Location Manager to receive location updates
+        if (mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+        else
+            System.out.println("No Network provider");
+
+        if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+        else
+            System.out.println("No GPS provider");
+
     }
 
     public void stopRecording() {
-        String filename = this.createCSVFile();
-        if (this.uploadFile()) {
-            this.getApplicationContext().deleteFile(filename);
+        File file = this.createCSVFile();
+        if (file.canRead()) {
+            if (this.uploadFile(file)) {
+                System.out.println("File uploaded!");
+                //this.getApplicationContext().deleteFile(file.getName());
+            }
         }
     }
 
@@ -101,18 +154,18 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
      *
      * @return String filename of the recently created file
      */
-    public String createCSVFile() {
+    public File createCSVFile() {
         FileOutputStream outputStream;
         String filename = "output.csv";
         try {
             outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
             outputStream.write(mStorageManager.generateOutput().getBytes());
             outputStream.close();
+            return getFileStreamPath(filename);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-
-        return filename;
     }
 
     /**
@@ -121,8 +174,27 @@ public class RecordActivity extends AppCompatActivity implements SensorEventList
      *
      * @return boolean Whether if it was a succesful upload or not
      */
-    public boolean uploadFile() {
-        return false;
+    public boolean uploadFile(File file) {
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                //"981161866060",
+                "us-east-1:8945e6af-0fa5-453b-84f7-f4e221bcf815", // Identity Pool ID
+                //"arn:aws:iam::981161866060:role/Cognito_BikeNYCUnauth_Role",
+                Regions.US_EAST_1 // Region
+        );
+
+        AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
+
+        TransferUtility transferUtility = new TransferUtility(s3, getApplicationContext());
+
+        TransferObserver observer = transferUtility.upload(
+                "bikenyc",     /* The bucket to upload to */
+                "output.csv",    /* The key for the uploaded object */
+                file        /* The file where the data to upload exists */
+        );
+
+        return true;
     }
 
     @Override
--- ProductStudio/app/src/main/java/com/bikenyc/productstudio/TrackingStorageManager.java
+++ ProductStudio/app/src/main/java/com/bikenyc/productstudio/TrackingStorageManager.java
@@ -1,8 +1,10 @@
 package com.bikenyc.productstudio;
 
 import android.hardware.SensorEvent;
+import android.location.Location;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Diego on 11/15/2016.
@@ -12,31 +14,128 @@ public class TrackingStorageManager {
 
     private ArrayList<float[]> mGyroscope;
     private ArrayList<float[]> mLinearAccelerometer;
-    private float[] mLocation;
+    private ArrayList<double[]> mLocation;
+
+    private Location mCurrentBestLocation;
 
     public TrackingStorageManager() {
         mGyroscope = new ArrayList<float[]>();
+        mLinearAccelerometer = new ArrayList<float[]>();
+        mLocation = new ArrayList<double[]>();
     }
 
     public void handleGyroscopeEvent(SensorEvent event) {
-        this.mGyroscope.add(event.values);
+        float[] values = new float[4];
+        values[0] = event.timestamp;
+        values[1] = event.values[0];
+        values[2] = event.values[1];
+        values[3] = event.values[2];
+        this.addValuesToGyroscope(values);
     }
 
     public void handleLinearAccelerometerEvent(SensorEvent event) {
-
+        float[] values = new float[4];
+        values[0] = event.timestamp;
+        values[1] = event.values[0];
+        values[2] = event.values[1];
+        values[3] = event.values[2];
+        this.addValuesToLinearAccelerometer(values);
     }
 
-    public void handleLocationEvent(SensorEvent event) {
+    public void handleLocationEvent(Location location) {
+        //if (!this.isBetterLocation(location))
+        //    return;
 
+        this.mCurrentBestLocation = location;
+        double[] values = new double[4];
+        values[0] = location.getElapsedRealtimeNanos();
+        values[1] = location.getLatitude();
+        values[2] = location.getLongitude();
+        values[3] = location.getAltitude();
+        this.addValuesToLocation(values);
     }
+
+    private void addValuesToGyroscope(float[] values) {
+        this.mGyroscope.add(values);
+    }
+    private void addValuesToLinearAccelerometer(float[] values) {this.mLinearAccelerometer.add(values);}
+    private void addValuesToLocation(double[] values) { this.mLocation.add(values); }
 
     public String generateOutput() {
         StringBuilder output = new StringBuilder();
+        output.append("[Gyroscope output(timestamp, x, y, z)]");
         for (float[] entry: mGyroscope) {
-            output.append(entry);
+            output.append(Arrays.toString(entry));
+            System.out.println(Arrays.toString(entry));
+        }
+
+        output.append("[Linear Accelerometer output(timestamp, x, y, z)]");
+        for (float[] entry: mLinearAccelerometer) {
+            output.append(Arrays.toString(entry));
+            System.out.println(Arrays.toString(entry));
+        }
+
+        output.append("[Location output(timestamp, Lat, Lng, Alt)]");
+        for (double[] entry: mLocation) {
+            output.append(Arrays.toString(entry));
+            System.out.println(Arrays.toString(entry));
         }
 
         return output.toString();
     }
 
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     */
+    protected boolean isBetterLocation(Location location) {
+        if (mCurrentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - mCurrentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - mCurrentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                mCurrentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
 }
